707818e7f0b6510a47725f0f58aea1142d1e7432
Steve Gilissen <info@pulselogic.be>
2021-06-24 13:54:01 +0200

    Fix description (Arduino IDE does not autosave...)

--- PMMacroKeyboard/PMMacroKeyboard.ino
+++ PMMacroKeyboard/PMMacroKeyboard.ino
@@ -1,6 +1,13 @@
 /*
    Poor Man's Macro Keyboard
+   -------------------------
+   By Steve Gilissen, 2021
 
+   Uses a simple membrane keypad, OLED and encoder for
+   various keyboard macro's. 
+
+   Made mostly for use with MMOs and some desktop apps,
+   but can be used for other things as well.
 
 */
 
@@ -82,8 +89,6 @@ void setup() {
 
   // Initiate Keyboard library
   Keyboard.begin();
-  
-
 }
 
 void loop() {
